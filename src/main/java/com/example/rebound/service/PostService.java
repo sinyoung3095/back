bcbed90a2e30d2860a24e948016284a10143af22
hjjung990927 <hjjung990927@gmail.com>
2025-08-13 00:13:51 +0900

    실패 경험담 게시글 목록#22

---
+++ src/main/java/com/example/rebound/service/PostService.java
@@ -0,0 +1,9 @@
+package com.example.rebound.service;
+
+import com.example.rebound.dto.PostCriteriaDTO;
+
+public interface PostService {
+
+//    목록
+    public PostCriteriaDTO getList(int page);
+}
