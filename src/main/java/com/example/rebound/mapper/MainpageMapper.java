9657064afdffa96316631aeea12da8f9e0343452
anna0121222 <anna012188@gmail.com>
2025-08-12 18:06:19 +0900

    0812(10)

--- src/main/java/com/example/rebound/mapper/MainpageMapper.java
+++ src/main/java/com/example/rebound/mapper/MainpageMapper.java
@@ -4,7 +4,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MainpageMapper {
-//    게시글 등록
-//    게시글 조회
+
 
 }
---
+++ src/test/java/com/example/rebound/mapper/MainpageMapperTests.java
@@ -0,0 +1,15 @@
+package com.example.rebound.mapper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class MainpageMapperTests {
+    @Autowired
+    private MainpageMapper mainpageMapper;
+
+
+}
