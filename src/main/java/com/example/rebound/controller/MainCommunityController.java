e63af9cbc74e844cb3dd4e3814eadebe4c464d93
anna0121222 <anna012188@gmail.com>
2025-08-12 14:41:02 +0900

    0812(2)

--- src/main/java/com/example/rebound/controller/MainCommunityController.java
+++ src/main/java/com/example/rebound/controller/MainCommunityController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/community-list/**")
-public class CommunityController {
+public class MainCommunityController {
 
     @GetMapping("list")
     public String goToCommunityList() {
