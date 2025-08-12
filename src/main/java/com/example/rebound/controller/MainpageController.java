6f810d5bcbde36c3be15e934e646cf018e3cab00
anna0121222 <anna012188@gmail.com>
2025-08-12 14:19:19 +0900

    0812

---
+++ src/main/java/com/example/rebound/controller/MainpageController.java
@@ -0,0 +1,15 @@
+package com.example.rebound.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main-page/**")
+public class MainpageController {
+    @GetMapping("page")
+    public String goToMainpage() {
+        return "/main-page/page";
+    }
+}
+
