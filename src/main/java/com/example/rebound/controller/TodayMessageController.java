d04977f21c29d79b6ca42fd21f401510c97e27c5
hjjung990927 <hjjung990927@gmail.com>
2025-08-12 15:57:45 +0900

    addcontroller

---
+++ src/main/java/com/example/rebound/controller/TodayMessageController.java
@@ -0,0 +1,14 @@
+package com.example.rebound.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/counselor-list/**")
+public class TodayMessageController {
+    @GetMapping("good-words")
+    public String goToGoodWords() {
+        return "/counselor-list/good-words";
+    }
+}
