51a1bd67a1693182d91f8bf124f04ad3eca52f34
anna0121222 <anna012188@gmail.com>
2025-08-19 00:12:03 +0900

    0818(3)

--- src/main/java/com/example/rebound/mapper/PaymentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.rebound.mapper;
-
-import com.example.rebound.dto.PaymentDTO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PaymentMapper {
-    public void goToPayment(PaymentDTO paymentDTO);
-}
--- src/main/java/com/example/rebound/service/PaymentService.java
+++
@@ -1,7 +0,0 @@
-package com.example.rebound.service;
-
-import com.example.rebound.dto.PaymentDTO;
-
-public interface PaymentService {
-    public void goToPayment(PaymentDTO paymentDTO);
-}
