f90f466a1921f904156857c9c527db319a2c99ca
sinyoung3095 <sinyoung3095@gmail.com>
2025-08-25 12:39:41 +0900

    작업중

--- src/main/resources/sql/counselor.sql
+++ src/main/resources/sql/counselor.sql
@@ -1,24 +1,17 @@
 
 create table tbl_counselor
 (
-    id                        bigint unsigned auto_increment
-        primary key,
-    counselor_name            varchar(255)                                                         not null,
-    counselor_email           varchar(255)                                                         not null,
-    counselor_phone_number    bigint                                                               not null,
-    counselor_password        varchar(255)                                                         not null,
-    counselor_address         varchar(255)                                                         not null,
-    counselor_status          enum ('active', 'inactive')                default 'active'          null,
-    counselor_approval_status enum ('approval', 'atmosphere', 'refusal') default 'atmosphere'      null,
-    created_date              datetime                                   default CURRENT_TIMESTAMP null,
-    updated_date              datetime                                   default CURRENT_TIMESTAMP null,
-    file_id                   bigint unsigned                                                      not null,
-    constraint counselor_email
-        unique (counselor_email),
-    constraint counselor_phone_number
-        unique (counselor_phone_number),
-    constraint fk_counselor_file
-        foreign key (file_id) references tbl_file (id)
+    id                        bigint unsigned auto_increment primary key,
+    counselor_name            varchar(255) not null,
+    counselor_email           varchar(255) not null unique,
+    counselor_phone_number    bigint not null unique,
+    counselor_password        varchar(255),
+    counselor_address         varchar(255),
+    counselor_status          enum ('active', 'inactive') default 'active',
+    counselor_approval_status enum ('approval', 'atmosphere', 'refusal') default 'atmosphere',
+    counselor_license_name varchar(255),
+    created_date              datetime default CURRENT_TIMESTAMP,
+    updated_date              datetime default CURRENT_TIMESTAMP
 );
 
 alter table tbl_counselor modify counselor_approval_status enum ('approval', 'await', 'refusal') default 'await';
@@ -28,13 +21,7 @@ values ('상담사이름5', 'test5@gmail.com', '06120123456789',
         '1234', '서울특별시 강남구 역삼로7길');
 
 select * from tbl_counselor;
-delete from tbl_counselor where id<20;
 
 alter table tbl_counselor modify counselor_password varchar(255);
 alter table tbl_counselor modify counselor_address varchar(255);
-alter table tbl_counselor modify file_id bigint unsigned;
-
-alter table tbl_counselor drop file_id;
-alter table tbl_counselor drop constraint fk_counselor_file;
-alter table tbl_counselor add counselor_license_name varchar(255);
 
