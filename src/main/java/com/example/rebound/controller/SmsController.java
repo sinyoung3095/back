7fc4b4be2aa61b3b65dd0d99cb363a30b933acc3
hjjung990927 <hjjung990927@gmail.com>
2025-08-28 09:03:45 +0900

    회원 이메일 찾기

--- src/main/java/com/example/rebound/controller/SmsController.java
+++ src/main/java/com/example/rebound/controller/SmsController.java
@@ -4,30 +4,28 @@ import com.example.rebound.service.MemberService;
 import com.example.rebound.service.SmsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.nurigo.sdk.message.model.Message;
-import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
-import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/sms/**")
 @Slf4j
 public class SmsController {
     private final SmsService smsService;
-    private final MemberService memberService;
 
-    @PostMapping("/send-one")
-    public SingleMessageSentResponse sendOne() {
-        Message message = new Message();
-        // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
-        message.setFrom("발신번호 입력");
-        message.setTo("수신번호 입력");
-        message.setText("한글 45자, 영자 90자 이하 입력되면 자동으로 SMS타입의 메시지가 추가됩니다.");
+    @PostMapping("/send")
+    public ResponseEntity<?> sendSms(@RequestBody Map<String, String> request) {
+        String memberPhoneNumber = request.get("memberPhoneNumber");
+        log.info("문자 전송 요청: {}", memberPhoneNumber);
 
-        SingleMessageSentResponse response = this.smsService.sendOne(new SingleMessageSendingRequest(message));
-        System.out.println(response);
+        String code = smsService.send(memberPhoneNumber);
 
-        return response;
+        return ResponseEntity.ok(Map.of("success", true, "code", code));
     }
 }
