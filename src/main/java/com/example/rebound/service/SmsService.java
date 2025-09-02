183886eee7de29c4ada9911deb7078b7cec2e5f4
hjjung990927 <hjjung990927@gmail.com>
2025-09-02 13:42:59 +0900

    final

--- src/main/java/com/example/rebound/service/SmsService.java
+++ src/main/java/com/example/rebound/service/SmsService.java
@@ -30,13 +30,15 @@ public class SmsService {
     public String send(String phoneNumber) {
         String code = createCode();
 
+//        각자 api key 입력
         String api_key = "";
         String api_secret = "";
         Message coolsms = new Message(api_key, api_secret);
 
         HashMap<String, String> params = new HashMap<>();
         params.put("to", phoneNumber);
-        params.put("from", "01055767406");
+//        전송하는 번호 입력
+        params.put("from", "");
         params.put("type", "SMS");
         params.put("text", "인증번호: " + code);
         params.put("app_version", "test app 1.2");
